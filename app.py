9d2bfe2b7a0adf39c882c36a45af9959fd9c522c
mirhami231100 <hasanalwi123@gmail.com>
2026-06-09 14:09:07 +0700

    Baru nambahin bagian home

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ home_page = st.Page('home.py', title='Home')
 input_page = st.Page('input.py', title='Input Pemasukan') #tambahkan icon="___"
 dashboard_page = st.Page('dashboard.py', title='Dashboard') #tambahkan icon="___"
 
-pg = st.navigation([input_page, dashboard_page])
+pg = st.navigation([home_page, input_page, dashboard_page])
 st.set_page_config(page_title='Self Cash Fow')
 pg.run()
--- home.py
+++ home.py
@@ -1,3 +1,23 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+st.title('🗂️SELF CASH FLOW🗂️')
+
+with st.container(border=False):
+    st.text('Selamat datang di website SELF CASH FLOW, web ini merupakan web pendukung untuk memanajamen keuangan teman-teman kedepannya.')
+
+with st.container(border=False):
+    st.text('Mari mulai menjadi pribadi yang peduli keuangan')
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        if st.button('Mulai Input'):
+            st.switch_page('input.py')
+    with col2:
+        if st.button('lihat Dashboard'):
+            st.switch_page('dashboard.py.py')
+    with col3:
+        if st.button('LIhat Video'):
+            st.switch_page('input.py')
